5620b39907011fb2c512661d6ade954ea9fbe8bb
sanchit1694 <Sanchitgangwar1694@gmail.com>
2025-07-08 18:17:38 +0530

    added serach and type filter and sorting logic in transaction table

--- actions/account.js
+++ actions/account.js
@@ -15,6 +15,39 @@ const serializeDecimal = (obj) => {
   return serialized;
 };
 
+export async function getAccountWithTransactions(accountId) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  const account = await db.account.findUnique({
+    where: {
+      id: accountId,
+      userId: user.id,
+    },
+    include: {
+      transactions: {
+        orderBy: { date: "desc" },
+      },
+      _count: {
+        select: { transactions: true },
+      },
+    },
+  });
+
+  if (!account) return null;
+
+  return {
+    ...serializeDecimal(account),
+    transactions: account.transactions.map(serializeDecimal),
+  };
+}
+
 
 export async function updateDefaultAccount(accountId) {
   try {
